7bf0088816e9996837cdf4b48e58e2797c0a01bb
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-23 13:30:03 -0400

    more comprehensive RINEX 2 OBS/NAV testing, default verbose

--- tests/test_all.py
+++ tests/test_all.py
@@ -11,25 +11,43 @@ from pyrinex import rinexobs, rinexnav
 #
 rdir=Path(__file__).parent
 
+# %% RINEX 2
+def test_obs2_allsat():
+    """./ReadRinex.py tests/demo.10o -u all -o tests/test2all.nc"""
 
-def test_obs2():
-    """./ReadRinex.py tests/demo.10o -o tests/test.nc"""
+    truth = xarray.open_dataset(rdir/'test2all.nc', group='OBS')
 
-    truth = xarray.open_dataset(rdir/'test.nc', group='OBS')
-
+# %% test reading all satellites
     for u in (None,'m','all',' ','',['G','R','S']):
         obs = rinexobs(rdir/'demo.10o', use=u)
         assert obs.equals(truth)
 
+def test_obs2_onesat():
+    """./ReadRinex.py tests/demo.10o -u G -o tests/test2G.nc"""
+
+    truth = xarray.open_dataset(rdir/'test2G.nc', group='OBS')
+
+    for u in ('G',['G']):
+        obs = rinexobs(rdir/'demo.10o', use=u)
+        assert obs.equals(truth)
+
+def test_obs2_twosat():
+    """./ReadRinex.py tests/demo.10o -u G R -o tests/test2GR.nc"""
+
+    truth = xarray.open_dataset(rdir/'test2GR.nc', group='OBS')
+
+    obs = rinexobs(rdir/'demo.10o', use=('G','R'))
+    assert obs.equals(truth)
+
 
 def test_nav2():
-    """./ReadRinex.py tests/demo.10n -o tests/test.nc"""
+    """./ReadRinex.py tests/demo.10n -o tests/test2all.nc"""
 
-    truth = xarray.open_dataset(rdir/'test.nc', group='NAV')
+    truth = xarray.open_dataset(rdir/'test2all.nc', group='NAV')
     nav = rinexnav(rdir/'demo.10n')
 
     assert nav.equals(truth)
-
+# %% RINEX 3
 
 def test_obs3():
     """./ReadRinex.py tests/demo3.10o -o tests/test3.nc"""
